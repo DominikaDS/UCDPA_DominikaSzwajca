68f75647cd3f7575115ed8e9a9be639ebcd85e17
dszwajca <dominika.szwajca@gmail.com>
2021-09-12 19:17:29 +0100

    Sorting, indexing, grouping

--- Python Project.py
+++ Python Project.py
@@ -27,6 +27,21 @@ import pandas as pd
 cars=pd.read_csv("norway_new_car_sales_by_make.csv")
 print(cars.head())
 
+#List methods - sorting brands in ascending order
+make=["Toyota", "Volkswagen", "Peugeot", "Ford", "Volvo"]
+make.sort()
+print(make)
+
+#List methods - indexing
+make=["Toyota", "Volkswagen", "Peugeot", "Ford", "Volvo"]
+quantity = [2884, 2521, 1029, 870, 693]
+make.index("Peugeot")
+print(quantity[2])
+
+#Grouping
+grouped_pc = cars.groupby(["Make"]).mean()
+print(grouped_pc)
+
 
 
 
